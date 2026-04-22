26e5ab4fa8d2771ed3de8de4248cface593b2859
Eureka <eurekaripfumelo@gmail.com>
2026-04-22 05:49:39 +0200

    Login configuration

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,3 +1,29 @@
 package controller;
 
+import dto.LoginRequest;
+import dto.LoginResponse;
+import dto.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth/user")
+public class AuthController{
+
+    @PostMapping("/registration")
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto user){
+
+        return ResponseEntity.ok(user);
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> userLogin(@RequestBody LoginRequest userLogin){
+
+        return ResponseEntity.ok(LoginResponse);
+    }
+
+
+}
+
 
---
+++ src/main/java/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package dto;
+
+public class LoginRequest {
+
+    private String username;
+    private String Password;
+}
